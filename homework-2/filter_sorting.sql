e6e6300f344536be674853760f9516ae0bf8c0d4
deus586 <73344615+deus586@users.noreply.github.com>
2023-12-08 11:23:34 +0300

    hw3

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -10,7 +10,7 @@ FROM orders ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 
-SELECT SUM(shipped_date - order_date) / COUNT(shipped_date) as avg
+SELECT SUM(shipped_date - order_date) / COUNT(shipped_date) as avg :: real
 FROM orders WHERE ship_country = 'Germany'
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
